46af96553ff6ccef04d56cb2d112d9b9a417b941
Omer Kenet <omerk@wix.com>
2025-06-28 18:48:33 +0300

    D

--- app/routes/kid-menu.tsx
+++ app/routes/kid-menu.tsx
@@ -272,7 +272,7 @@ export default function KidMenu() {
               You&apos;ve completed your weekly challenge and unlocked Dunkin’ Donuts meal.
             </p>
           </div>
-          <img src="dd.png" className={styles.heroImg} />
+          <img src="Dunkin.png" className={styles.heroImg} />
         </div>
       </section>
 
